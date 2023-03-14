b38d94f0990e9331ef7eba8ade73a110a09e8396
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-14 22:03:33 +0100

    Add basic PID

---
+++ pid_basic.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor
+from ev3dev2.sound import Sound
+
+from typing import Tuple
+
+##################
+#                #
+#    SETTINGS    #
+#                #
+##################
+
+FORWARD_SPEED = 30
+
+CONSTANT_P = 8.0
+CONSTANT_I = 0.9
+CONSTANT_D = 0.003
+
+HISTORY_LOSS = 0.5
+
+###################
+#                 #
+#    CONSTANTS    #
+#                 #
+###################
+
+SOUND_VOLUME = 100
+
+LEFT = 0
+RIGHT = 1
+
+#####################
+#                   #
+#    PERIPHERALS    #
+#                   #
+#####################
+
+sound = Sound()
+button = TouchSensor(INPUT_3)
+
+left_motor = LargeMotor(OUTPUT_A)
+right_motor = LargeMotor(OUTPUT_B)
+
+motors = [left_motor, right_motor]
+
+left_sensor = ColorSensor(INPUT_1)
+right_sensor = ColorSensor(INPUT_2)
+
+sensors = [left_sensor, right_sensor]
+
+# move_tank = MoveTank(OUTPUT_A, OUTPUT_B)
+
+######################
+#                    #
+#    MAIN PROGRAM    #
+#                    #
+######################
+
+
+def speak(message: str) -> None:
+    sound.speak(message)
+    print(message)
+
+
+def work() -> None:
+    integral = 0.0
+    last_error = 0.0
+    derivative = 0.0
+
+    while True:
+        if button.is_pressed:
+            handle_button_pressed()
+        else:
+            iterate(integral, last_error, derivative)
+
+
+def handle_button_pressed() -> None:
+    stop()
+    speak('STOP')
+    button.wait_for_released()
+    button.wait_for_bump()
+    speak('START')
+
+
+def iterate(integral: float, last_error: float, derivative: float) -> Tuple[float, float, float]:
+    error = left_sensor.reflected_light_intensity - \
+        right_sensor.reflected_light_intensity
+
+    integral = HISTORY_LOSS * integral + error
+    derivative = error - last_error
+    last_error = error
+
+    turn_speed = CONSTANT_P * error + CONSTANT_I * integral + CONSTANT_D * derivative
+
+    left_motor.on(FORWARD_SPEED + turn_speed)
+
+    left_motor.on(FORWARD_SPEED - turn_speed)
+
+    return integral, last_error, derivative
+
+
+def stop() -> None:
+    for motor in motors:
+        motor.stop()
+
+
+def main() -> None:
+    sound.set_volume(SOUND_VOLUME)
+    speak('READY')
+
+    button.wait_for_bump()
+    speak('START')
+
+    try:
+        work()
+    except KeyboardInterrupt as e:
+        stop()
+        raise e
+
+
+if __name__ == '__main__':
+    main()
