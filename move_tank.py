6cefefd320698589b06642c02dadae12d4835c9c
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-14 21:08:13 +0100

    Refactor move_tank.py

--- move_tank.py
+++ move_tank.py
@@ -1,24 +1,109 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_B, LineFollowErrorTooFast, SpeedPercent
-from ev3dev2.sensor import INPUT_1, INPUT_3
+from ev3dev2.sound import Sound
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent, LineFollowErrorTooFast, follow_for_ms  # type: ignore
+
+##################
+#                #
+#    SETTINGS    #
+#                #
+##################
+
+CONSTANT_P = 11.3
+CONSTANT_I = 0.05
+CONSTANT_D = 3.2
+
+SPEED_PERCENT = SpeedPercent(30)
+
+CHECK_BUTTON_INTERVAL_MS = 1000
+
+###################
+#                 #
+#    CONSTANTS    #
+#                 #
+###################
+
+SOUND_VOLUME = 100
+
+LEFT = 0
+RIGHT = 1
+
+#####################
+#                   #
+#    PERIPHERALS    #
+#                   #
+#####################
+
+sound = Sound()
+button = TouchSensor(INPUT_3)
+
+left_sensor = ColorSensor(INPUT_1)
+right_sensor = ColorSensor(INPUT_2)
+
+sensors = [left_sensor, right_sensor]
+
+move_tank = MoveTank(OUTPUT_A, OUTPUT_B)
+
+######################
+#                    #
+#    MAIN PROGRAM    #
+#                    #
+######################
+
+
+def speak(message: str) -> None:
+    sound.speak(message)
+    print(message)
+
+
+def work() -> None:
+    while True:
+        if button.is_pressed:
+            handle_button_pressed()
+        else:
+            iterate()
+
+
+def handle_button_pressed() -> None:
+    stop()
+    speak('STOP')
+    button.wait_for_released()
+    button.wait_for_bump()
+    speak('START')
+
+
+def iterate() -> None:
+    try:
+        move_tank.follow_line(
+            kp=CONSTANT_P,
+            ki=CONSTANT_I,
+            kd=CONSTANT_D,
+            speed=SPEED_PERCENT,
+            follow_for=follow_for_ms,  # type: ignore
+            ms=CHECK_BUTTON_INTERVAL_MS
+        )
+    except LineFollowErrorTooFast:
+        move_tank.stop()
+        raise
+
+
+def stop() -> None:
+    move_tank.stop()
 
 
 def main() -> None:
-    tank = MoveTank(left_motor_port=OUTPUT_A, right_motor_port=OUTPUT_B)
-    tank.cs = ColorSensor(INPUT_1)
+    sound.set_volume(SOUND_VOLUME)
+    speak('READY')
 
-    button = TouchSensor(INPUT_3)
     button.wait_for_bump()
+    speak('START')
 
     try:
-        tank.follow_line(
-            kp=11.3, ki=0.05, kd=3.2,
-            speed=SpeedPercent(30)
-        )
-    except LineFollowErrorTooFast:
-        tank.stop()
-        raise
+        work()
+    except KeyboardInterrupt as e:
+        stop()
+        raise e
 
 
 if __name__ == '__main__':
