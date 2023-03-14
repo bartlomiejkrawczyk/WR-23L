44fd41f93b461d9108ba077ca864ad8399159285
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-14 22:34:59 +0100

    Ged rid of warning in base.py

--- base.py
+++ base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B  # , MoveTank, SpeedPercent
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor
 from ev3dev2.sound import Sound
