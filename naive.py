4dce262c4cdf1b287d671ca2c84e55723bf35b02
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-14 20:57:55 +0100

    Prepare naive line follower

--- naive.py
+++ naive.py
@@ -1,42 +1,120 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_B
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, MoveTank, SpeedPercent
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor
+from ev3dev2.sound import Sound
 
-SPEED_STRAIGHT = 30
-SPEED_TURN = 30
+from time import sleep
+
+##################
+#                #
+#    SETTINGS    #
+#                #
+##################
+
+FORWARD_SPEED = 30
+TURN_SPEED = 40
+
+SLEEP = 1
+
+###################
+#                 #
+#    CONSTANTS    #
+#                 #
+###################
+
+SOUND_VOLUME = 100
+
+LEFT = 0
+RIGHT = 1
+
+#####################
+#                   #
+#    PERIPHERALS    #
+#                   #
+#####################
+
+sound = Sound()
+button = TouchSensor(INPUT_3)
+
+left_sensor = ColorSensor(INPUT_1)
+right_sensor = ColorSensor(INPUT_2)
+
+sensors = [left_sensor, right_sensor]
+
+move_tank = MoveTank(OUTPUT_A, OUTPUT_B)
+
+######################
+#                    #
+#    MAIN PROGRAM    #
+#                    #
+######################
 
 
-def iteration(tank: MoveTank, left: ColorSensor, right: ColorSensor) -> None:
-
-    if left.color == ColorSensor.COLOR_WHITE and right.color == ColorSensor.COLOR_WHITE:
-        tank.on(left_speed=SPEED_STRAIGHT, right_speed=SPEED_STRAIGHT)
-    elif left.color == ColorSensor.COLOR_BLACK and right.color == ColorSensor.COLOR_BLACK:
-        tank.on(left_speed=SPEED_STRAIGHT, right_speed=SPEED_STRAIGHT)
-    elif left.color == ColorSensor.COLOR_BLACK and right.color == ColorSensor.COLOR_WHITE:
-        tank.on(left_speed=-SPEED_TURN, right_speed=SPEED_TURN)
-    elif left.color == ColorSensor.COLOR_WHITE and right.color == ColorSensor.COLOR_BLACK:
-        tank.on(left_speed=SPEED_TURN, right_speed=-SPEED_TURN)
+def speak(message: str) -> None:
+    sound.speak(message)
+    print(message)
 
 
-def loop(tank: MoveTank, left: ColorSensor, right: ColorSensor) -> None:
+def work() -> None:
     while True:
-        iteration(tank, left, right)
+        if button.is_pressed:
+            handle_button_pressed()
+        else:
+            iterate()
+
+
+def handle_button_pressed() -> None:
+    stop()
+    speak('STOP')
+    button.wait_for_released()
+    button.wait_for_bump()
+    speak('START')
+
+
+def iterate() -> None:
+    if left_sensor.color == right_sensor.color:
+        move_tank.on(
+            SpeedPercent(FORWARD_SPEED),
+            SpeedPercent(FORWARD_SPEED)
+        )
+    elif left_sensor.color != ColorSensor.COLOR_BLACK and right_sensor.color != ColorSensor.COLOR_BLACK:
+        move_tank.on(
+            SpeedPercent(FORWARD_SPEED),
+            SpeedPercent(FORWARD_SPEED)
+        )
+    elif left_sensor.color != ColorSensor.COLOR_WHITE and right_sensor.color == ColorSensor.COLOR_WHITE:
+        move_tank.on(
+            SpeedPercent(-FORWARD_SPEED - TURN_SPEED),
+            SpeedPercent(FORWARD_SPEED + TURN_SPEED)
+        )
+    elif left_sensor.color == ColorSensor.COLOR_WHITE and right_sensor.color != ColorSensor.COLOR_WHITE:
+        move_tank.on(
+            SpeedPercent(FORWARD_SPEED + TURN_SPEED),
+            SpeedPercent(-FORWARD_SPEED - TURN_SPEED)
+        )
+    elif left_sensor.color == ColorSensor.COLOR_NOCOLOR or right_sensor.color == ColorSensor.COLOR_NOCOLOR:
+        move_tank.off()
+
+    sleep(SLEEP)
+
+
+def stop() -> None:
+    move_tank.off()
 
 
 def main() -> None:
-    tank = MoveTank(left_motor_port=OUTPUT_A, right_motor_port=OUTPUT_B)
-    color_sensor_left = ColorSensor(INPUT_1)
-    color_sensor_right = ColorSensor(INPUT_2)
+    sound.set_volume(SOUND_VOLUME)
+    speak('READY')
 
-    button = TouchSensor(INPUT_3)
     button.wait_for_bump()
+    speak('START')
 
     try:
-        loop(tank, color_sensor_left, color_sensor_right)
-    except KeyboardInterrupt:
-        tank.stop()
-        raise
+        work()
+    except KeyboardInterrupt as e:
+        stop()
+        raise e
 
 
 if __name__ == '__main__':
