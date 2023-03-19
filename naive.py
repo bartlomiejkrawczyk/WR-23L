190fc1ac6b0609ab1a2fac873c24cb99e270995a
Bartłomiej Krawczyk <01158771@pw.edu.pl>
2023-03-19 18:45:48 +0100

    Update naive code not to call detect color so many times

--- naive.py
+++ naive.py
@@ -73,27 +73,31 @@ def handle_button_pressed() -> None:
 
 
 def iterate() -> None:
-    if left_sensor.color == right_sensor.color:
+    colors = (
+        left_sensor.color,
+        right_sensor.color
+    )
+    if colors[LEFT] == colors[RIGHT]:
         move_tank.on(
             SpeedPercent(FORWARD_SPEED),
             SpeedPercent(FORWARD_SPEED)
         )
-    elif left_sensor.color != ColorSensor.COLOR_BLACK and right_sensor.color != ColorSensor.COLOR_BLACK:
+    elif colors[LEFT] != ColorSensor.COLOR_BLACK and colors[RIGHT] != ColorSensor.COLOR_BLACK:
         move_tank.on(
             SpeedPercent(FORWARD_SPEED),
             SpeedPercent(FORWARD_SPEED)
         )
-    elif left_sensor.color != ColorSensor.COLOR_WHITE and right_sensor.color == ColorSensor.COLOR_WHITE:
+    elif colors[LEFT] != ColorSensor.COLOR_WHITE and colors[RIGHT] == ColorSensor.COLOR_WHITE:
         move_tank.on(
             SpeedPercent(-FORWARD_SPEED - TURN_SPEED),
             SpeedPercent(FORWARD_SPEED + TURN_SPEED)
         )
-    elif left_sensor.color == ColorSensor.COLOR_WHITE and right_sensor.color != ColorSensor.COLOR_WHITE:
+    elif colors[LEFT] == ColorSensor.COLOR_WHITE and colors[RIGHT] != ColorSensor.COLOR_WHITE:
         move_tank.on(
             SpeedPercent(FORWARD_SPEED + TURN_SPEED),
             SpeedPercent(-FORWARD_SPEED - TURN_SPEED)
         )
-    elif left_sensor.color == ColorSensor.COLOR_NOCOLOR or right_sensor.color == ColorSensor.COLOR_NOCOLOR:
+    elif colors[LEFT] == ColorSensor.COLOR_NOCOLOR or colors[RIGHT] == ColorSensor.COLOR_NOCOLOR:
         move_tank.off()
 
     sleep(SLEEP_SECONDS)
