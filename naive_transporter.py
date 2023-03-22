7cbe2241988186784dfe690984905012e217599e
Bartłomiej Krawczyk <bartlomiej.krawczyk2.stud@pw.edu.pl>
2023-03-22 19:06:35 +0100

    WIP

--- naive_transporter.py
+++ naive_transporter.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B
 from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3
 from ev3dev2.sensor.lego import TouchSensor, ColorSensor
 from ev3dev2.sound import Sound
@@ -14,25 +14,8 @@ from time import sleep
 #                #
 ##################
 
-MIN_FORWARD_SPEED = 5
-MAX_FORWARD_SPEED = 10
-
-FORWARD_SPEED_CORRECTION = (
-    (MAX_FORWARD_SPEED - MIN_FORWARD_SPEED) / MAX_FORWARD_SPEED
-)
-
-CONSTANT_P = 4.0
-CONSTANT_I = 0.01
-CONSTANT_D = 4.0
-
-HISTORY_LOSS = 0.5
-
-AMPLIFIER = 0.1
-
-FROM = ColorSensor.COLOR_RED
-TO = ColorSensor.COLOR_GREEN
-
-SLEEP_MS = 0.025 # 0.1
+MIN_FORWARD_SPEED = 10
+MAX_FORWARD_SPEED = 20
 
 ###################
 #                 #
@@ -45,6 +28,8 @@ SOUND_VOLUME = 100
 LEFT = 0
 RIGHT = 1
 
+FROM = ColorSensor.COLOR_RED
+TO = ColorSensor.COLOR_GREEN
 
 #####################
 #                   #
@@ -65,11 +50,6 @@ right_sensor = ColorSensor(INPUT_2)
 
 sensors = [left_sensor, right_sensor]
 
-motor = MediumMotor(OUTPUT_C)
-
-motor.on(10)
-sleep(1)
-motor.on(0)
 
 ################
 #              #
@@ -96,9 +76,6 @@ def speak(message: str) -> None:
 
 
 def work() -> None:
-    integral = 0.0
-    last_error = 0
-
     state = FOLLOW_LINE_UNTIL_FROM
     desired_color = FROM
 
@@ -106,11 +83,10 @@ def work() -> None:
         if button.is_pressed:
             handle_button_pressed()
         else:
-            state, desired_color = iterate(
-                state, desired_color, integral, last_error)
+            state, desired_color = iterate(state, desired_color)
 
 
-def iterate(state: int, desired_color: int, integral: float, last_error: int) -> Tuple[int, int]:
+def iterate(state: int, desired_color: int) -> Tuple[int, int]:
     colors = detect_colors()
 
     desired_color = 1
@@ -127,32 +103,21 @@ def iterate(state: int, desired_color: int, integral: float, last_error: int) ->
         if colors[LEFT] == desired_color:
             state = FOLLOW_LINE_UNTIL_DETECTED_OBJECT
     
-    # print('colors = ', colors)
-    # print('state = ', state)
-
-    try:
-        integral, last_error = follow_line(integral, last_error)
-    except Exception as e:
-        print(e)
+    print('colors = ', colors)
+    print('state = ', state)
     
 
-    # if state in [FOLLOW_LINE_UNTIL_FROM, FOLLOW_LINE_UNTIL_TO, FOLLOW_LINE_UNTIL_DETECTED_OBJECT]:
-    #     try:
-    #         integral, last_error = follow_line(integral, last_error)
-    #     except Exception as e:
-    #         print(e)
-    # elif state == TURN_RIGHT:
-    #     left_motor.on(0)
-    #     right_motor.on(0)
-    #     sleep(1)
-    #     left_motor.on(-MIN_FORWARD_SPEED)
-    #     right_motor.on(MIN_FORWARD_SPEED)
-    # elif state == TURN_LEFT:
-    #     left_motor.on(0)
-    #     right_motor.on(0)
-    #     sleep(1)
-    #     left_motor.on(MIN_FORWARD_SPEED)
-    #     right_motor.on(MIN_FORWARD_SPEED)
+    if state in [FOLLOW_LINE_UNTIL_FROM, FOLLOW_LINE_UNTIL_TO, FOLLOW_LINE_UNTIL_DETECTED_OBJECT]:
+        try:
+            follow_line()
+        except Exception as e:
+            print(e)
+    elif state == TURN_RIGHT:
+        left_motor.on(-MIN_FORWARD_SPEED)
+        right_motor.on(MIN_FORWARD_SPEED)
+    elif state == TURN_LEFT:
+        left_motor.on(MIN_FORWARD_SPEED)
+        right_motor.on(MIN_FORWARD_SPEED)
 
     return state, desired_color
 
@@ -166,39 +131,34 @@ def handle_button_pressed() -> None:
 
 
 def detect_colors() -> Tuple[int, int]:
-    left_sensor._ensure_mode(ColorSensor.MODE_COL_COLOR)
-    right_sensor._ensure_mode(ColorSensor.MODE_COL_COLOR)
-    # sleep(0.1)
+    left_sensor.mode = ColorSensor.MODE_COL_COLOR
+    right_motor.mode = ColorSensor.MODE_COL_COLOR
     return (
         left_sensor.color,
         right_sensor.color
     )
 
 
-def follow_line(integral: float, last_error: int) -> Tuple[float, int]:
-    left_sensor._ensure_mode(ColorSensor.MODE_COL_REFLECT)
-    right_sensor._ensure_mode(ColorSensor.MODE_COL_REFLECT)
-    sleep(0.1)
-    error = left_sensor.reflected_light_intensity - \
-        right_sensor.reflected_light_intensity
-
-    integral = HISTORY_LOSS * integral + error
-    derivative = error - last_error
-    last_error = error
-
-    turn_speed = CONSTANT_P * error + CONSTANT_I * integral + CONSTANT_D * derivative
-
-    forward_speed = max(
-        MIN_FORWARD_SPEED,
-        MAX_FORWARD_SPEED - FORWARD_SPEED_CORRECTION * abs(turn_speed)
+def follow_line() -> None:
+    colors = (
+        left_sensor.color,
+        right_sensor.color
     )
-
-    left_motor.on(forward_speed + AMPLIFIER * turn_speed)
-    right_motor.on(forward_speed - AMPLIFIER * turn_speed)
-    sleep(SLEEP_MS)
-
-    return integral, last_error
-
+    if colors[LEFT] == colors[RIGHT]:
+        left_motor.on(MIN_FORWARD_SPEED)
+        right_motor.on(MIN_FORWARD_SPEED)
+    elif colors[LEFT] != ColorSensor.COLOR_BLACK and colors[RIGHT] != ColorSensor.COLOR_BLACK:
+        left_motor.on(MIN_FORWARD_SPEED)
+        right_motor.on(MIN_FORWARD_SPEED)
+    elif colors[LEFT] != ColorSensor.COLOR_WHITE and colors[RIGHT] == ColorSensor.COLOR_WHITE:
+        left_motor.on(-MIN_FORWARD_SPEED)
+        right_motor.on(MIN_FORWARD_SPEED)
+    elif colors[LEFT] == ColorSensor.COLOR_WHITE and colors[RIGHT] != ColorSensor.COLOR_WHITE:
+        left_motor.on(MIN_FORWARD_SPEED)
+        right_motor.on(-MIN_FORWARD_SPEED)
+    elif colors[LEFT] == ColorSensor.COLOR_NOCOLOR or colors[RIGHT] == ColorSensor.COLOR_NOCOLOR:
+        left_motor.on(0)
+        right_motor.on(0)
 
 def stop() -> None:
     for motor in motors:
